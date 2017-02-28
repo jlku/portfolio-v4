58bcc4ce0c0d3dfe3f33e08c5cdd942798fd946f
Adam Noffsinger <adam.noffsinger@uber.com>
2017-02-28 09:57:25 -0800

    Integrated lazy load library

--- js/script-common.js
+++ js/script-common.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     var header = $('.header');
     var scrolledClass = "header-scrolled";
 
+    var bLazy = new Blazy({
+        // load image 100px before you reach it
+        offset: 100
+    });
+
     $(window).scroll(function() {
         if ($(this).scrollTop()) {
             header.addClass(scrolledClass);
