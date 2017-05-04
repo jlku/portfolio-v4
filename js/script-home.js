940c27fb1bb99855ed29279ba3fc1b274322b4eb
Adam Noffsinger <adam.noffsinger@uber.com>
2017-05-03 22:47:43 -0700

    Refactored home script

--- js/script-home.js
+++ js/script-home.js
@@ -1,24 +1,36 @@
 $(document).ready(function() {
 
-    // initialize mouse parallax
+    // initialize parallax.js
     var scene = document.getElementById('scene');
     var parallax = new Parallax(scene);
 
+    // variables
     var body = $("body");
+    var wrapperHome = $(".wrapper-home");
 
-    var allShapes = $("#scene")
+    var wholeScene = $("#scene")
+    var shapesTriangle = $("[class*='scene-triangle']");
+
+    var intro = $('.intro');
     var introGreeting = $('.intro-greeting');
     var introBio = $('.intro-bio');
-    var intro = $('.intro');
-    var shapesTriangle = $("[class*='scene-triangle']");
-    var aboutButton = $(".home-header-about");
-    var aboutCloseButton = $(".button-close-x");
-    var about = $("#about");
-    var aboutContainer = $(".about-container");
-    var content = $(".wrapper-home");
     var introArrow = $(".intro-arrow");
 
-    // about hover
+    var workItem = $(".work-item");
+
+    var about = $("#about");
+    var aboutContainer = $(".about-container");
+    var aboutCloseButton = $(".button-close-x");
+    var aboutButton = $(".home-header-about");
+
+    var buttonCloseLegOne = $(".button-close-x-leg-one");
+    var buttonCloseLegTwo = $(".button-close-x-leg-two");
+
+    // levers
+    var workTileLoadStagger = 200;
+    var workTileLoadPoint = 400;
+
+    // about link hover
     aboutButton.hover(function() {
         body.addClass("nightmode-background");
         shapesTriangle.addClass("nightmode-shapes-triangle");
@@ -26,109 +38,90 @@ $(document).ready(function() {
         introBio.addClass("nightmode-text-light");
         aboutButton.addClass("nightmode-text-dark");
     }, function() {
-
         var hasClass = about.hasClass("about-visible");
-
         if (hasClass == false) {
             body.removeClass("nightmode-background");
             shapesTriangle.removeClass("nightmode-shapes-triangle");
             introGreeting.removeClass("nightmode-text-dark");
             introBio.removeClass("nightmode-text-light");
             aboutButton.removeClass("nightmode-text-dark");
-
-        } else {
-            // do nothing
         }
-
     });
 
-    // about click
+    // about link click
     aboutButton.click(function() {
         body.addClass("is-no-scroll");
-        allShapes.addClass("scene-hidden")
+        wholeScene.addClass("scene-hidden")
         about.addClass("about-visible");
-        content.addClass("is-hidden");
+        wrapperHome.addClass("is-hidden");
         aboutButton.addClass("is-hidden home-header-about-hidden");
         aboutCloseButton.addClass("is-visible button-close-x-rotated");
         aboutContainer.addClass("about-container-loaded");
-        $(".button-close-x > div:nth-of-type(1)").addClass("button-close-x-rotate-right");
-        $(".button-close-x > div:nth-of-type(2)").addClass("button-close-x-rotate-left");
+        buttonCloseLegOne.addClass("button-close-x-rotate-right");
+        buttonCloseLegTwo.addClass("button-close-x-rotate-left");
     });
 
-    // about close
+    // about close click
     aboutCloseButton.click(function() {
-
         aboutCloseButton.removeClass("is-visible button-close-x-rotated");
-        $(".button-close-x > div:nth-of-type(1)").removeClass("button-close-x-rotate-right");
-        $(".button-close-x > div:nth-of-type(2)").removeClass("button-close-x-rotate-left");
+        buttonCloseLegOne.removeClass("button-close-x-rotate-right");
+        buttonCloseLegTwo.removeClass("button-close-x-rotate-left");
         about.removeClass("about-visible");
         aboutContainer.removeClass("about-container-loaded");
         introGreeting.removeClass("nightmode-text-dark");
         introBio.removeClass("nightmode-text-light");
 
-
         setTimeout(function(){
             body.removeClass("nightmode-background is-no-scroll");
-            allShapes.removeClass("scene-hidden");
+            wholeScene.removeClass("scene-hidden");
             shapesTriangle.removeClass("nightmode-shapes-triangle");
-            content.removeClass("is-hidden");
+            wrapperHome.removeClass("is-hidden");
             aboutButton.removeClass("is-hidden nightmode-text-dark home-header-about-hidden");
-
         }, 300);
     });
 
-    // escape button functionality
+    // about escape button close
     document.addEventListener('keyup', function(e) {
-
         if (e.keyCode == 27) {
-            allShapes.removeClass("scene-hidden")
             aboutCloseButton.removeClass("is-visible button-close-x-rotated");
-            $(".button-close-x > div:nth-of-type(1)").removeClass("button-close-x-rotate-right");
-            $(".button-close-x > div:nth-of-type(2)").removeClass("button-close-x-rotate-left");
+            buttonCloseLegOne.removeClass("button-close-x-rotate-right");
+            buttonCloseLegTwo.removeClass("button-close-x-rotate-left");
             about.removeClass("about-visible");
             aboutContainer.removeClass("about-container-loaded");
-            shapesTriangle.removeClass("nightmode-shapes-triangle");
+            introGreeting.removeClass("nightmode-text-dark");
+            introBio.removeClass("nightmode-text-light");
 
             setTimeout(function(){
                 body.removeClass("nightmode-background is-no-scroll");
-                content.removeClass("is-hidden");
+                wholeScene.removeClass("scene-hidden");
+                shapesTriangle.removeClass("nightmode-shapes-triangle");
+                wrapperHome.removeClass("is-hidden");
                 aboutButton.removeClass("is-hidden nightmode-text-dark home-header-about-hidden");
-                introGreeting.removeClass("nightmode-text-dark");
-                introBio.removeClass("nightmode-text-light");
             }, 300);
         }
     });
 
-
-    var limit = 20;  /* scrolltop value when opacity should be 0 */
-    var loadDelay = 200;
+    // scroll events
     $(window).on('scroll', function() {
        var scrollPosition = $(this).scrollTop();
 
        if (scrollPosition > 0) {
            introArrow.addClass('intro-arrow-hidden');
-           console.log("working");
        } else {
            introArrow.removeClass('intro-arrow-hidden');
        }
 
-       if (scrollPosition > 400) {
-           $(".work-item").each(function(i, el) {
-               var $this = $(this);
+       if (scrollPosition > workTileLoadPoint) {
+           workItem.each(function(i, el) {
+               var tile = $(this);
                setTimeout(function() {
-                   $this.addClass('work-item-loaded');
-               }, i * loadDelay);
+                   tile.addClass('work-item-loaded');
+               }, i * workTileLoadStagger);
            });
        }
 
-
-
-       aboutButton.css('transform', 'translateY(' + scrollPosition/-3 + 'px)');
-       intro.css('transform', 'translateY(' + scrollPosition/-3 + 'px)');
-       allShapes.css('transform', 'translateY(' + scrollPosition/-5 + 'px)');
-
-    //    if (scrollPosition <= limit) {
-    //        introArrow.css({ 'opacity' : (1 - scrollPosition/limit) });
-    //    }
+       aboutButton.css('transform', 'translateY(' + scrollPosition/-2.5 + 'px)');
+       intro.css('transform', 'translateY(' + scrollPosition/-2.5 + 'px)');
+       wholeScene.css('transform', 'translateY(' + scrollPosition/-5 + 'px)');
     });
 });
